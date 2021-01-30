ece1af556a86bea1bccffe0a760c12a70a0a2e51
Jairo Julià Bravo <jairocnx@gmail.com>
2021-01-30 21:02:34 +0100

    Values check

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -5,6 +5,7 @@ const EXECUTION_STATUS = core.getInput('EXECUTION_STATUS');
 const BUILD_STATUS = core.getInput('BUILD_STATUS');
 const DEPLOY_STATUS = core.getInput('DEPLOY_STATUS');
 
+
 console.log(SYNTAX_STATUS);
 console.log(EXECUTION_STATUS);
 console.log(BUILD_STATUS);
